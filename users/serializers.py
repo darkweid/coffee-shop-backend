cc47027021629a0234d4a6a9ff23eac100193b7c
Mark Shamanskii <darkweid713@gmail.com>
2024-12-01 15:47:52 +0500

    Feat: added token due registration for future users verification

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,5 @@
+import uuid
+
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
@@ -14,8 +16,24 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
 
+    def save(self, **kwargs):
+        user = super().save(**kwargs)
+        user.verification_token = str(uuid.uuid4())
+        user.save()
+
+        # ToDo: use send email in production
+        # send_mail(
+        #     'Verify your account',
+        #     f'Your verification token is {user.verification_token}',
+        #     'noreply@example.com',
+        #     [user.email],
+        #     fail_silently=False,
+        # )
+        print(f'\nYour verification token is {user.verification_token}\n')
+        return user
+
 
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name')
+        fields = ('id', 'email', 'first_name', 'last_name', 'is_active')
