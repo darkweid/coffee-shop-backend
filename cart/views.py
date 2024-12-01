0db94fb355981dcd3ba9472a452f1794053c11d4
Mark Shamanskii <darkweid713@gmail.com>
2024-12-01 20:56:30 +0500

    Feat: added cart views

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Cart
+from .serializers import CartSerializer
+
+
+class CartAddView(generics.CreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CartDeleteView(generics.DestroyAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CartClearView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        user = request.user
+        user.cart.all().delete()
+        return Response({"message": "Cart cleared successfully"}, status=204)
