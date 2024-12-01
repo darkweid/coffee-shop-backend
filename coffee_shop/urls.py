57b9015c99ff43341c3080add5d3f464e1e67c5d
Mark Shamanskii <darkweid713@gmail.com>
2024-12-01 20:57:03 +0500

    Feat: includes cart and orders urls

--- coffee_shop/urls.py
+++ coffee_shop/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('products.urls')),
+    path('api/', include('orders.urls')),
+    path('api/', include('cart.urls')),
 
 ]
