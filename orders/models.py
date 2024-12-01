1cf10bac0b9fea3fe1a86e0a1a9d405f64fe0215
Mark Shamanskii <darkweid713@gmail.com>
2024-12-01 20:55:21 +0500

    Feat: added model order

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="orders")
+    quantity = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.email}"
