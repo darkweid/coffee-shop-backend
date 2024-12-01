68e6fd6ac1329e90d41199cf46a366208967af35
Mark Shamanskii <darkweid713@gmail.com>
2024-12-01 15:44:08 +0500

    Feat: added urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import UserRegistrationView, UserDetailView
+from .views import (UserRegistrationView, UserDetailView, UserVerificationView,
+                    UserAuthenticationView, UserListView, MeView)
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
-    path('me/', UserDetailView.as_view(), name='user-detail'),
+    path('me/', MeView.as_view(), name='me'),
+    path('verification/', UserVerificationView.as_view(), name='user-verification'),
+    path('authentication/', UserAuthenticationView.as_view(), name='authentication'),
+    path('users/', UserListView.as_view(), name='users'),
+    path('user/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+
 ]
