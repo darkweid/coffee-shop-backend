48bcb070b07ed8ad302c4e80a8e834075f44e6a0
Mark Shamanskii <darkweid713@gmail.com>
2024-12-01 20:54:48 +0500

    Feat: added orders views

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Order
+from .serializers import OrderSerializer
+from .filters import OrderFilter
 
-# Create your views here.
+
+class OrderListCreateView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = OrderFilter
+
+
+class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
