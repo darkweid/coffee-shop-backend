acb27acec3303ac7a9bc26e920d57e8830164119
Mark Shamanskii <darkweid713@gmail.com>
2024-12-01 15:43:55 +0500

    Feat: added ApiViews for users

--- users/views.py
+++ users/views.py
@@ -1,10 +1,17 @@
 from rest_framework import generics, permissions
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
+from django_filters import rest_framework as filters
+
+from .filters import UserFilter
 from .serializers import UserRegistrationSerializer, UserDetailSerializer
+from rest_framework import views, status
 
 User = get_user_model()
+
 
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
@@ -21,9 +28,56 @@ class UserRegistrationView(generics.CreateAPIView):
             'access': str(refresh.access_token),
         })
 
-class UserDetailView(generics.RetrieveAPIView):
-    serializer_class = UserDetailSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
-    def get_object(self):
-        return self.request.user
+class UserVerificationView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        token = request.data.get("token")
+        if not token:
+            return Response({"detail": "Token is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(verification_token=token)
+            user.is_active = True
+            user.verification_token = None
+            user.save()
+            return Response({"detail": "User verified successfully."}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({"detail": "Invalid token."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserAuthenticationView(TokenObtainPairView):
+    """JWT Authentication view."""
+    pass
+
+
+class MeView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        })
+
+
+class UserListView(generics.ListAPIView):
+    """List all users."""
+    queryset = User.objects.all().order_by('-id')
+    serializer_class = UserDetailSerializer
+    permission_classes = [IsAdminUser]
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = UserFilter
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        print(queryset)  # Выведем в консоль, чтобы увидеть фильтрованные данные
+        return queryset
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """Retrieve, update or delete a user."""
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
+    permission_classes = [IsAdminUser]
