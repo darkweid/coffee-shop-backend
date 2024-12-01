7839926881e7fba7e7ac38910f9c552dcff35cbb
Mark Shamanskii <darkweid713@gmail.com>
2024-12-01 20:55:56 +0500

    Feat: added cart model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cart")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="cart")
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"Cart item {self.product.name} for {self.user.email}"
