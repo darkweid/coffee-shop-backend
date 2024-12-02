b642036fe30511d32106d02966f7a7bee848edb2
Mark Shamanskii <darkweid713@gmail.com>
2024-12-02 14:06:01 +0500

    Feat: added API Docs endpoints

--- coffee_shop/urls.py
+++ coffee_shop/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from .views import home, StaticInfoView
 
@@ -30,8 +31,13 @@ urlpatterns = [
     path('api/', include('cart.urls')),
     path('support-chat/', TemplateView.as_view(template_name='chat.html')),
     path('api/static-info/', StaticInfoView.as_view(), name='static-info'),
+
     # JWT tokens
-    path('access/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/access/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # API Docs
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # schema OpenAPI
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Swagger UI
 
 ]
